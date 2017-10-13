4aea2697d96c66dd1b4a6912365c339db92ae909
simplebrightman <nghuyong@163.com>
2017-10-13 22:03:13 +0800

    hide private info

--- log.py
+++ log.py
@@ -10,8 +10,8 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)  # 输出到控制台的log等级的开关
 # 创建该handler的formatter
 logger_format = logging.Formatter(
-        fmt='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
+    fmt='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 console_handler.setFormatter(logger_format)
 logger.addHandler(console_handler)
 mail_handler = SMTPHandler(
@@ -19,9 +19,8 @@ mail_handler = SMTPHandler(
     fromaddr='nghuyong@163.com',
     toaddrs='1013975672@qq.com',
     subject='这是一封proypool项目发来的邮件',
-    credentials=('nghuyong', 'huyong199596'))
+    credentials=('account', 'password'))
 
 mail_handler.setLevel(logging.ERROR)
 mail_handler.setFormatter(logger_format)
 logger.addHandler(mail_handler)
-
